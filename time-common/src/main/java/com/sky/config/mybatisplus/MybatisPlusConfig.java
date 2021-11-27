9b7dc4c9226b22a7c712d5a9dc7a935b4635d0c0
sukey <799116474@qq.com>
2021-11-27 16:41:33 +0800

    相关框架配置（redis、mybatis...）

---
+++ time-common/src/main/java/com/sky/config/mybatisplus/MybatisPlusConfig.java
@@ -0,0 +1,44 @@
+package com.sky.config.mybatisplus;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.core.incrementer.IKeyGenerator;
+import com.baomidou.mybatisplus.extension.incrementer.H2KeyGenerator;
+import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("com.sky.mapper")
+public class MybatisPlusConfig {
+
+    /**
+     * 分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        paginationInterceptor.setDbType(DbType.MYSQL);
+        return paginationInterceptor;
+    }
+
+    /**
+     * 乐观锁
+     */
+
+    @Bean
+    public OptimisticLockerInterceptor optimisticLockerInterceptor() {
+        return new OptimisticLockerInterceptor();
+    }
+
+    /**
+     * 主键序列的生成
+     * ID_WORK 数字
+     * ID_WORK_STR 字符串
+     */
+    @Bean
+    public IKeyGenerator iKeyGenerator() {
+        return new H2KeyGenerator();
+    }
+}
