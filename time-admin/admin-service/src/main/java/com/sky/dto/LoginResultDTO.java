7eebfb86d7f622e64962fcf0cb5a9709da3f8978
sukey <799116474@qq.com>
2021-11-27 16:45:59 +0800

    补充文件

---
+++ time-admin/admin-service/src/main/java/com/sky/dto/LoginResultDTO.java
@@ -0,0 +1,36 @@
+package com.sky.dto;
+
+import com.sky.domain.SysMenu;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.List;
+
+/**
+ * 登录的返回值
+ *
+ * @author sky
+ * @create 2021-11-21 16:28
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginResultDTO {
+
+    /**
+     * 登录产生的token
+     */
+    private String token;
+    /**
+     * 前端的菜单数据
+     */
+    private List<SysMenu> menus;
+    /**
+     * 权限数据
+     */
+    private List<SimpleGrantedAuthority> authorities;
+
+}
