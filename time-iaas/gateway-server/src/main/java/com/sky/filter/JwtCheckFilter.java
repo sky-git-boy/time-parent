11c3212d68f4dbde943ffbc886e5b3bf648c31b3
sukey <799116474@qq.com>
2021-11-27 16:38:17 +0800

    网关服务器添加过滤器（token）

---
+++ time-iaas/gateway-server/src/main/java/com/sky/filter/JwtCheckFilter.java
@@ -0,0 +1,101 @@
+package com.sky.filter;
+
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Set;
+
+/**
+ * 判断该token 是否存在
+ *
+ * @author sky
+ */
+@Component
+public class JwtCheckFilter implements GlobalFilter, Ordered {
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    @Value("${no.require.urls:/admin/login}")
+    private Set<String> noRequireTokenUris;
+
+    /**
+     * 过滤器拦截到用户的请求后做啥
+     */
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        // 1 : 该接口是否需要token 才能访问
+        if (!isRequireToken(exchange)) {
+            return chain.filter(exchange);// 不需要token ,直接放行
+        }
+        // 2: 取出用户的token
+        String token = getUserToken(exchange);
+
+        // 3 判断用户的token 是否有效
+        if (StringUtils.isEmpty(token)) {
+            return buildeNoAuthorizationResult(exchange);
+        }
+
+        Boolean hasKey = redisTemplate.hasKey(token);
+        if (hasKey != null && hasKey) {
+            return chain.filter(exchange);// token有效 ,直接放行
+        }
+        return buildeNoAuthorizationResult(exchange);
+    }
+
+    /**
+     * 给用户响应一个没有token的错误
+     */
+    private Mono<Void> buildeNoAuthorizationResult(ServerWebExchange exchange) {
+        ServerHttpResponse response = exchange.getResponse();
+        response.getHeaders().set("Content-Type", "application/json");
+        response.setStatusCode(HttpStatus.UNAUTHORIZED);
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("error", "NoAuthorization");
+        jsonObject.put("errorMsg", "Token is Null or Error");
+        DataBuffer wrap = response.bufferFactory().wrap(jsonObject.toJSONString().getBytes());
+        return response.writeWith(Flux.just(wrap));
+    }
+
+    /**
+     * 从 请求头里面获取用户的token
+     */
+    private String getUserToken(ServerWebExchange exchange) {
+        String token = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+        return token == null ? null : token.replace("bearer ", "");
+    }
+
+    /**
+     * 判断该 接口是否需要token
+     */
+    private boolean isRequireToken(ServerWebExchange exchange) {
+        String path = exchange.getRequest().getURI().getPath();
+        if (noRequireTokenUris.contains(path)) {
+            return false; // 不需要token
+        }
+        return Boolean.TRUE;
+    }
+
+
+    /**
+     * 拦截器的顺序
+     */
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
