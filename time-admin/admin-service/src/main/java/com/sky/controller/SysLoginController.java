c2ed64b961d3bc71f4523624baeacd7a794f65fd
sukey <799116474@qq.com>
2021-12-14 15:04:14 +0800

    impl 登录日志

--- time-admin/admin-service/src/main/java/com/sky/controller/SysLoginController.java
+++ time-admin/admin-service/src/main/java/com/sky/controller/SysLoginController.java
@@ -1,10 +1,20 @@
 package com.sky.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.sky.aspectj.annotation.Log;
+import com.sky.aspectj.enums.BusinessStatus;
+import com.sky.aspectj.enums.BusinessType;
+import com.sky.aspectj.enums.OperatorType;
 import com.sky.constants.Constants;
 import com.sky.constants.HttpStatus;
+import com.sky.domain.LogOperInfo;
 import com.sky.dto.LoginResultDTO;
 import com.sky.params.LoginInParams;
+import com.sky.service.LogOperInfoService;
 import com.sky.service.SysLoginService;
+import com.sky.utils.AddressUtils;
+import com.sky.utils.IpUtils;
+import com.sky.utils.ServletUtils;
 import com.sky.vo.R;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -13,6 +23,10 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.spring.web.json.Json;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
 
 /**
  * 登录控制器
@@ -27,10 +41,14 @@ public class SysLoginController {
     @Autowired
     private SysLoginService loginService;
 
+    @Autowired
+    private LogOperInfoService logOperInfoService;
+
     @PostMapping("/login")
     @ApiOperation(value = "后台管理人员登录")
     public R login(@RequestBody @Validated LoginInParams params) {
         R ajax = R.success();
+        LogOperInfo logOperInfo = createLogOperInfo(params);
         try {
 
             // 设置的登陆返回值
@@ -39,9 +57,33 @@ public class SysLoginController {
             ajax.put(Constants.MENUS, dto.getMenus());
             ajax.put(Constants.AUTHORITIES, dto.getAuthorities());
 
+            logOperInfo.setJsonResult(JSON.toJSONString(R.success()));
         } catch (Exception e) {
             ajax = R.error(HttpStatus.ERROR, "用户名或密码不正确");
+            logOperInfo.setJsonResult(JSON.toJSONString(ajax));
+            logOperInfo.setStatus(String.valueOf(BusinessStatus.FAIL.ordinal()));
+            logOperInfo.setErrorMsg(e.getMessage());
         }
+        logOperInfoService.save(logOperInfo);
         return ajax;
+    }
+
+    private LogOperInfo createLogOperInfo(LoginInParams params) {
+        LogOperInfo logOperInfo = new LogOperInfo();
+        logOperInfo.setTitle("登录");
+        logOperInfo.setBusinessType(String.valueOf(BusinessType.LOGIN.ordinal()));
+        logOperInfo.setOperParam("账号: " + params.getUsername());
+
+        logOperInfo.setStatus(String.valueOf(BusinessStatus.SUCCESS.ordinal()));
+
+        String ip = IpUtils.getIpAddr(ServletUtils.getRequest());
+        logOperInfo.setOperIp(ip);
+        String address = AddressUtils.getRealAddressByIP(ip);
+        logOperInfo.setOperLocation(address);
+
+        logOperInfo.setOperTime(new Date());
+        logOperInfo.setOperatorType(String.valueOf(OperatorType.OTHER.ordinal()));
+
+        return logOperInfo;
     }
 }
