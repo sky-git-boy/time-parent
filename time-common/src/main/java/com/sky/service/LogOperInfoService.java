ee96d91cb92f4e6c23f946b4150443bdc8e7341b
sukey <799116474@qq.com>
2021-12-14 15:03:57 +0800

    update 操作日志

--- time-common/src/main/java/com/sky/service/LogOperInfoService.java
+++ time-common/src/main/java/com/sky/service/LogOperInfoService.java
@@ -2,6 +2,8 @@ package com.sky.service;
 
 import com.sky.domain.LogOperInfo;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.sky.dto.LogOperInfoDTO;
+import com.sky.vo.DataGridView;
 
 /**
  * @author sky
@@ -9,5 +11,16 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface LogOperInfoService extends IService<LogOperInfo> {
 
+    // 分页查询
+    DataGridView listForPage(LogOperInfoDTO operLogDto);
+
+    // 删除
+    int deleteOperLogByIds(Long[] infoIds);
+
+    // 清除全部
+    int clearAllOperLog();
+
+    // 插入操作日志
+    int insertOperLog(LogOperInfo operLog);
 
 }
--- time-common/src/main/java/com/sky/service/impl/LogOperInfoServiceImpl.java
+++ time-common/src/main/java/com/sky/service/impl/LogOperInfoServiceImpl.java
@@ -1,8 +1,15 @@
 package com.sky.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.sky.dto.LogOperInfoDTO;
+import com.sky.vo.DataGridView;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.Arrays;
 import java.util.List;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -16,5 +23,38 @@ import com.sky.service.LogOperInfoService;
  */
 @Service
 public class LogOperInfoServiceImpl extends ServiceImpl<LogOperInfoMapper, LogOperInfo> implements LogOperInfoService {
+    @Autowired
+    private LogOperInfoMapper operInfoMapper;
 
+    @Override
+    public DataGridView listForPage(LogOperInfoDTO operLogDto) {
+        QueryWrapper<LogOperInfo> qw = new QueryWrapper<>();
+        Page<LogOperInfo> page = new Page<>(operLogDto.getPageNum(), operLogDto.getPageSize());
+        qw.like(StringUtils.isNotBlank(operLogDto.getOperBy()), LogOperInfo.COL_OPER_BY, operLogDto.getOperBy());
+        qw.like(StringUtils.isNotBlank(operLogDto.getTitle()), LogOperInfo.COL_TITLE, operLogDto.getTitle());
+        qw.eq(StringUtils.isNotBlank(operLogDto.getBusinessType()), LogOperInfo.COL_BUSINESS_TYPE, operLogDto.getBusinessType());
+        qw.eq(StringUtils.isNotBlank(operLogDto.getStatus()), LogOperInfo.COL_STATUS, operLogDto.getStatus());
+        qw.ge(null != operLogDto.getBeginTime(), LogOperInfo.COL_OPER_TIME, operLogDto.getBeginTime());
+        qw.le(null != operLogDto.getEndTime(), LogOperInfo.COL_OPER_TIME, operLogDto.getEndTime());
+        this.operInfoMapper.selectPage(page, qw);
+        return new DataGridView(page.getTotal(), page.getRecords());
+    }
+
+    @Override
+    public int deleteOperLogByIds(Long[] infoIds) {
+        if(infoIds != null && infoIds.length > 0) {
+            return this.operInfoMapper.deleteBatchIds(Arrays.asList(infoIds));
+        }
+        return 0;
+    }
+
+    @Override
+    public int clearAllOperLog() {
+        return this.operInfoMapper.delete(null);
+    }
+
+    @Override
+    public int insertOperLog(LogOperInfo operLog) {
+        return this.operInfoMapper.insert(operLog);
+    }
 }
