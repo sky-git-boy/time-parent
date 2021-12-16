261745728d2ca90bde3bcc32b97214be249585d3
sukey <799116474@qq.com>
2021-12-16 17:56:08 +0800

    个人反省添加校验

--- time-member/member-service/src/main/java/com/sky/dto/TimeJournalDTO.java
+++ time-member/member-service/src/main/java/com/sky/dto/TimeJournalDTO.java
@@ -9,6 +9,8 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+
 /**
  * 个人日志DTO
  * @author sky
@@ -31,12 +33,14 @@ public class TimeJournalDTO extends BaseDTO {
      * 标题
      */
     @ApiModelProperty(value = "标题")
+    @NotNull(message = "标题不能为空")
     private String title;
 
     /**
      * 描述
      */
     @ApiModelProperty(value = "描述")
+    @NotNull(message = "描述不能为空")
     private String description;
 
 }
