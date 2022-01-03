403484c255b49116ee09188ff22f29cca4fa8813
sukeyu <799116474@qq.com>
2022-01-03 11:04:54 +0800

    惩罚机制实现

--- time-member/member-service/src/main/java/com/sky/job/RulesJobService.java
+++ time-member/member-service/src/main/java/com/sky/job/RulesJobService.java
@@ -3,6 +3,7 @@ package com.sky.job;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.sky.constants.Constants;
 import com.sky.domain.TimeRules;
+import com.sky.dto.MessageDTO;
 import com.sky.mapper.TimeRulesMapper;
 import com.sky.mapper.TimeTaskMapper;
 import com.sky.server.WebSocketServer;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -38,19 +40,30 @@ public class RulesJobService {
         List<TimeRules> rulesList = rulesMapper.selectList(new LambdaQueryWrapper<TimeRules>().eq(TimeRules::getStatus, Constants.STATUS_TRUE));
 
         rulesList.forEach(rule -> {
-            // 今日需完成数
-            int needCount = Integer.parseInt(rule.getNeedCount());
-
-            // 获取用户今日完成数
-            int doneCount = taskMapper.getUserTodayDoneCount(rule.getUserId());
-            // 如果今日完成数大于等于应完成数
-            if (doneCount >= needCount) {
+            // 如果截至时间小于当前时间，说明已超时 -> 发送超时惩罚
+            if (rule.getDeadLine().before(new Date())) {
                 try {
                     // 发送消息
-                    WebSocketServer.sendInfo(rule.getRewardContent(), String.valueOf(rule.getUserId()));
+                    WebSocketServer.sendInfo(new MessageDTO(rule.getPunishmentContent(), Constants.NOT_UNIQUE), String.valueOf(rule.getUserId()));
                     updateList.add(rule);
                 } catch (IOException e) {
                     e.printStackTrace();
+                }
+            } else {
+                // 今日需完成数
+                int needCount = Integer.parseInt(rule.getNeedCount());
+
+                // 获取用户今日完成数
+                int doneCount = taskMapper.getUserTodayDoneCount(rule.getUserId());
+                // 如果今日完成数大于等于应完成数
+                if (doneCount >= needCount) {
+                    try {
+                        // 发送消息
+                        WebSocketServer.sendInfo(new MessageDTO(rule.getRewardContent(), Constants.UNIQUE), String.valueOf(rule.getUserId()));
+                        updateList.add(rule);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         });
